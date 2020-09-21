fb10989b8738c8d47223bd50fd216ece4319171d
Dmitriy Suchkov <dsuchkov@jet.su>
2020-09-21 11:32:40 +0300

    7.1 draw obstacle border

--- core/space_garbage.py
+++ core/space_garbage.py
@@ -4,11 +4,10 @@ import random
 from core import globals
 from core.helpers import _get_random_position, sleep
 from core.settings import GARBAGE_FRAMES_DIR
-from vendor.curses_tools import draw_frame
+from vendor.curses_tools import draw_frame, get_frame_size
+from vendor.obstacles import Obstacle
 
 import asyncio
-
-from vendor.obstacles import Obstacle
 
 
 async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
@@ -20,10 +19,19 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
 
     row = 0
 
+    frame_size = get_frame_size(garbage_frame)
+
     while row < rows_number - 1:
+
         draw_frame(canvas, row, column, garbage_frame)
+
+        obstacle = Obstacle(row, column, *frame_size)
+        bounding_box_frame = obstacle.get_bounding_box_frame()
+        draw_frame(canvas, row, column, bounding_box_frame)
+
         await asyncio.sleep(0)
         draw_frame(canvas, row, column, garbage_frame, negative=True)
+        draw_frame(canvas, row, column, bounding_box_frame, negative=True)
         row += speed
 
 
@@ -42,7 +50,7 @@ async def fill_orbit_with_garbage(canvas):
         globals.coroutines.append(
             fly_garbage(canvas=canvas, column=col, garbage_frame=frame)
         )
-        globals.obstacles.append(
-            Obstacle(row=)
-        )
+        # globals.obstacles.append(
+        #     Obstacle(row=)
+        # )
         await sleep(20)
